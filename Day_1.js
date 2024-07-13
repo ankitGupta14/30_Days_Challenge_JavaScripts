d5a1e3c0988012f83c12b071e8f14c98686a906f
Ankit <ag2526049@gmail.com>
2024-07-13 21:57:07 +0530

    first commit

---
+++ Day_1.js
@@ -0,0 +1,62 @@
+//  Variables and Data Types:-------
+
+// Activity 1: Variable Declaration
+
+// Task 1: Declare a variable using var, assign it a number, and log the value to the console.
+var Day1 = 45;
+console.log(Day1)
+
+// Task 2: Declare a variable using let, assign it a string, and log the value to the console.
+let Task2 = "Chai aur code"
+console.log(Task2)
+
+// Activity 2: Constant Declaration
+
+// Task 3: Declare a variable using const, assign it a boolean value, and log the value to the console.
+const javaScriptFun = true;
+console.log(javaScriptFun);
+
+// Activity 3: Data Types
+
+// Task 4: Create variables of different data types (number, string, boolean, object, array) and log each variable's type using the typeof operator.
+
+// Number
+const KillAge = 34
+console.log(typeof KillAge)
+
+// String
+const KillName = "Kill"
+console.log(typeof KillName)
+
+// Boolean
+const killTruth = true
+console.log(typeof killTruth)
+
+// Object
+const KillPerson ={
+    name: "Kill",
+    age: 34,
+    isAlive: true
+}
+console.log(typeof KillPerson)
+
+// Array 
+const Fruits = ["apple", "orange", "banana"]
+console.log(typeof Fruits)
+
+// Activity 4: Reassigning Variables
+
+// Task 5: Declare a variable using let, assign it an initial value, reassign a new value, and log both values to the console.
+
+let ChaiaurCode = 342;
+console.log(ChaiaurCode)
+ChaiaurCode = 452;
+console.log(ChaiaurCode)
+
+// Activity 5: Understanding const
+
+// Task 6: Try reassigning a variable declared with const and observe the error.
+
+const ChaiisHot = "Chai is Ready "
+console.log(ChaiisHot)
+ChaiisHot = "Chai is not Ready" // Error = Assignment to constant variable. 
